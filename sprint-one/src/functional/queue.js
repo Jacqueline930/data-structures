fcda61237a74bfe8c4d09bdf63f3582674be89d1
Jacqueline930 <miriam.alvarez.p96@gmail.com>
2018-01-12 20:02:21 -0600

    worked on functional queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,48 +1,28 @@
 var Queue = function(){
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
-  var queue = [];
-
-  // Implement the methods below
-  var length =0;
+  var storage = {};
+  var counter = 0;
+  var nextUp = 0;
 
   someInstance.enqueue = function(value){
-      queue[length] = value;
-      length++;
+      storage[counter] = value;
+      counter++;
   };
 
   someInstance.dequeue = function(){
     //same as return queue.shift();
-    var v = queue[0];
-    shift();
-    length--;
-    return v;
+    var val = storage[nextUp];
+    delete storage[nextUp];
+    nextUp++;
+    return val;
   };
 
   someInstance.size = function(){
-    return length;
+    if(counter - nextUp > 0) {
+      return counter - nextUp;
+    } else {
+      return 0;
+    }
   };
-
-  someInstance.print = function() {
-      var str = "[";
-      for (var i = 0; i < length; i++) {
-          str += queue[i]
-          if (i != length - 1) {
-            str += " ,";
-          }
-      }
-      str += "]";
-      console.log(str);
-  };
-
   return someInstance;
-
-  function shift() {
-      for (var i = 0; i < queue.length - 1; i++) {
-          queue[i] = queue[i+1];
-      }
-      //queue.pop();
-
-  }
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var total = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[total] = value;
+    total++;
   };
 
   someInstance.pop = function() {
+    total--;
+    var value = storage[total];
+    delete storage[total];
+    return value
   };
 
   someInstance.size = function() {
+    if (total > 0) {
+      return total;
+    } else {
+      return 0;
+    }
   };
-
+  
   return someInstance;
 };
